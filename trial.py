62bde944cbc352e3b8a5edb83a96e27d4a062c1d
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-12 14:09:08 +0300

    submitted

---
+++ trial.py
@@ -0,0 +1,13 @@
+# intro objects relatioships.
+
+'''
+python offers a high degree of flexibility in defining classes objects.
+with python, we can assign attributes and mthods classes and objects as well as add new aatributes to objects even after 
+they have been instantiated.
+
+additionally we can create classes that inherits attributea and methods from
+a parent enabling children classes to receive those features.
+
+However some relatioships between classes and objects are not as straightforward as parent-child relationships.
+
+'''
